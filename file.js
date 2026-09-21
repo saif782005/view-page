6f9765e64112991492d29665ca7b68537c85b8d0
saif782005 <111455105+saif782005@users.noreply.github.com>
2026-09-21 18:32:58 +0300

    Refactor dark theme toggle logic in file.js

--- file.js
+++ file.js
@@ -2,12 +2,12 @@ let dark = document.querySelector("#dark-butt");
 if (localStorage.getItem("theme") === "dark") {
   document.body.classList.add("dark");
 }
-
+dark.textContent = "light";
 dark.addEventListener("click", function () {
   let isDark = document.body.classList.toggle("dark");
   console.log(isDark);
   localStorage.setItem("theme", isDark ? "dark" : "light");
-  dark.textContent = "light";
+  
   if (isDark === true) {
     dark.textContent = "light";
   } else {
